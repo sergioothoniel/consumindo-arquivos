d8fe43d1bef3f0abaa0772a2c35d033c1cd8856a
sergioothoniel <sergioothoniel@hotmail.com>
2022-10-18 17:06:52 -0300

    update: adicionado método dinâmico para caracter de quebra de linha

--- src/utils/fileSystem.js
+++ src/utils/fileSystem.js
@@ -1,11 +1,14 @@
 import fs from "fs/promises"
+import os from "os"
+
+const newLineMetaChar = os.EOL
 
 export async function readFile(path){   
     
     try {
 
         const data = await fs.readFile(path, "utf8")               
-        return data.split("\n").map(item=>item && JSON.parse(item.trim())).filter(item=>!!item)
+        return data.split(newLineMetaChar).map(item=>item && JSON.parse(item.trim())).filter(item=>!!item)
         
     } catch (error) {
         console.error(error)        
